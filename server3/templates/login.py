d12043bd910752a66d56d37b41db58f82b9929c4
jiuhuandao <a13070175@163.com>
2016-09-09 12:48:29 +0800

    Create login.py

---
+++ server3/templates/login.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+       <head>
+       <meta charset="utf-8">
+       <title>my profile</title>
+       </head>
+<body>
+  
+       <h1>Hello world!</h1>  
+       <img src="/static?file=doge.gif"/>
+       <img src="/static?file=doge1.jpg"/>
+       <img src="/static?file=doge2.gif"/>
+       <a href="/login">Login</a>
+</body>
+
+</html>
